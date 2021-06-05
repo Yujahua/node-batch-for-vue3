6c2388d285ca673104812c46111c04428cea954a
yujahua <yujahua@163.com>
2021-06-05 23:34:20 +0800

    feature(core):add TODOS and public method purpose

---
+++ src/engine/core.js
@@ -0,0 +1,28 @@
+/**
+ * lib: 223-core.js
+ * Date: 2021-6-5
+ * Purpose: Transform grammar
+
+ */
+(function(){
+    // Provide tools and tec, is main and private
+    function core() {
+        // TODO
+    }
+    /**
+     * As its name, analysis and report.
+     * Call private functions to acheive it.
+     */
+    function analysisReport() {
+        // TODO
+    }
+    // Provide for public
+    function _223Core() {
+        // TODO
+        
+        return {
+            analysisReport:analysisReport
+        }
+    }
+
+})()
